fb58222999d73bde0c28a44b34bfa73fef5a0e34
Joe Beaudoin <jmcsmith@gmail.com>
2025-06-09 20:50:30 -0400

    attempt to use FetchDescriptor to limit properties to fetch and prefetch others. Instruments indicates that the fetch returns 104k+ records in 474.14ms and then has a ton of DataFaults on Relationships

--- SwiftDataTest/ContentView.swift
+++ SwiftDataTest/ContentView.swift
@@ -12,22 +12,35 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @AppStorage("hasopenedbefore", store: UserDefaults.standard) var hasOpenedBefore = false
     @State private var showingOnboarding = false
-    @Query(sort: \Card.name) private var cards:  [Card]
+    @State private var cards: [Card] = []
     
-//    @Query(filter: #Predicate<Card> { c in
-//        c.name.localizedStandardContains("Sliver")
-//    }, sort: \Card.name) private var cards: [Card]
+    private func fetchCards() {
+        var descriptor = FetchDescriptor<Card>(predicate:
+                                               // #Predicate<Card> { c in c.name.localizedStandardContains("Sliver")}
+                                               nil
+                                               , sortBy: [SortDescriptor(\Card.name)]
+        )
+        descriptor.propertiesToFetch = [\Card.name]
+        descriptor.relationshipKeyPathsForPrefetching = [\Card.set] // Prefetch 'set'
+        do {
+            cards = try modelContext.fetch(descriptor)
+        } catch {
+            print("Failed to fetch cards: \(error)")
+        }
+    }
 
     var body: some View {
        NavigationStack {
            List(cards) { card in
                VStack(alignment: .leading) {
                    Text(card.name)
-                   Text(card.set?.name ?? "")
-                       .font(.caption)
+//                   Text(card.set?.name ?? "Unknown Set")
+//                       .font(.caption)
                }
+               
            }
-           .navigationTitle(Text("\(cards.count) Cards"))
+           .listRowSeparator(.hidden)
+           .navigationTitle("\(cards.count) Cards")
         }
        .fullScreenCover(isPresented: $showingOnboarding) {
            OnboardingView()
@@ -36,9 +49,9 @@ struct ContentView: View {
             if !hasOpenedBefore {
                 showingOnboarding.toggle()
             }
+            fetchCards()
         }
     }
 
 
 }
-
